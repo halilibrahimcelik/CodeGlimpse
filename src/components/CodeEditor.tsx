37cff6dff83bf59a882864c563efec4e18403d8a
halilibrahimcelik <halil.celik139@gmail.com>
2023-09-12 12:51:58 +0300

    feat:added prettier to editor

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -2,31 +2,51 @@ import Editor from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 import { useRef } from "react";
 import { GridLoader } from "react-spinners";
-import prettier from "prettier";
-import parser from "prettier/parser-babel";
+import prettier from "prettier/standalone";
+import babelPlugin from "prettier/plugins/babel";
+import estreePlugin from "prettier/plugins/estree";
 
 interface CodeEditorProps {
   initialValue: string;
   onChange: (value: string) => void;
 }
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
-  const editorRef = useRef<string | null | editor.IStandaloneCodeEditor>("");
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
 
   const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
+    editorRef.current = editor;
     editor.onDidChangeModelContent(() => {
       onChange(editor.getValue());
-      editorRef.current = editor.getValue();
     });
 
     editor.getModel()?.updateOptions({
-      tabSize: 2,
+      tabSize: 1,
     });
   };
-  const handleFormat = () => {
-    console.log(editorRef.current);
+  const handleFormat = async () => {
+    //unformated code
+    const unformated = editorRef?.current?.getValue() as string;
+
+    try {
+      //formated code
+      const formated = await prettier.format(unformated, {
+        parser: "babel",
+        plugins: [babelPlugin, estreePlugin],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      });
+
+      //set formated code to editor
+      editorRef?.current?.setValue(formated);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <>
+      <button onClick={handleFormat}>Format</button>
+
       <Editor
         theme="vs-dark"
         height={"500px"}
@@ -46,8 +66,6 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
         }}
         loading={<GridLoader color="#36d7b7" margin={3} />}
       />
-
-      <button onClick={handleFormat}>Format</button>
     </>
   );
 };
