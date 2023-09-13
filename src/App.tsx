417764720a6c31a2b209c489afb855ed6a1321bb
halilibrahimcelik <halil.celik139@gmail.com>
2023-09-13 12:41:11 +0300

    feat:added bundle file

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,15 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import Form from "./components/Form";
-
-import * as esbuild from "esbuild-wasm";
 import CodeEditor from "./components/CodeEditor";
 import DarkMode from "./components/DarkMode";
 import { useAppDispatch } from "./app/store";
 import { toggleDarkMode } from "./app/features/globalSlice";
 import PreviwCode from "./components/PreviwCode";
 
-export type EsbuildService = esbuild.Service;
-
 function App() {
-  const serviceRef = useRef<EsbuildService | null>(null);
   const dispatch = useAppDispatch();
 
-  const startService = async () => {
-    const response = await esbuild.startService({
-      worker: true,
-      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
-    });
-    serviceRef.current = response;
-  };
-
   useEffect(() => {
-    startService();
     dispatch(toggleDarkMode());
   }, []);
 
@@ -31,7 +17,7 @@ function App() {
     <main className="dark:bg-primaryBg bg-white ">
       <DarkMode />
       <CodeEditor initialValue="const a=1" />
-      <Form serviceRef={serviceRef} />
+      <Form />
 
       <PreviwCode />
     </main>
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,38 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
-import { EsbuildService } from "../App";
-import { unpkgPathPlugin } from "../plugins/unpkg-path-plugin";
-import { fetchPlugin } from "../plugins/fetch-plugin";
+
 import { useSelector } from "react-redux";
 import { getSelectedInput, setCode } from "../app/features/globalSlice";
 import { useAppDispatch } from "../app/store";
+import bundler from "../bundler";
 
-type Props = {
-  serviceRef: React.MutableRefObject<EsbuildService | null>;
-};
-const env = ["process", "env", "NODE_ENV"].join(".");
-const Form = ({ serviceRef }: Props) => {
+const Form = () => {
   const dispatch = useAppDispatch();
   const input = useSelector(getSelectedInput);
 
   const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    console.log(input);
-    if (!serviceRef?.current) {
-      return;
-    }
 
-    const result = await serviceRef.current.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        [env]: '"production"',
-        global: "window",
-      },
-    });
-    dispatch(setCode(result.outputFiles[0].text));
+    const output = await bundler(input);
+
+    dispatch(setCode(output));
   };
 
   return (
