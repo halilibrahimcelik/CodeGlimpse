bd9cc267968973d8c244e64c0e2d21d0baf841b0
halilibrahimcelik <halil.celik139@gmail.com>
2023-09-27 14:18:53 +0300

    feat:added no-opeartion case

--- src/components/CodeCell.tsx
+++ src/components/CodeCell.tsx
@@ -20,32 +20,38 @@ const CodeCell = (props: Cell) => {
 
   const getCumulativeContent = createSelector([getCells], (cell) => {
     const orderedCells = cell.order.map((id) => cell.data[id]);
-    const cumulativeContent = [
-      `
-      import _React from 'react';
-      import _ReactDom from 'react-dom';
 
-      function show(input){
-        const root = document.querySelector("#root");
-        if(typeof input === 'object'){
-          if(input.$$typeof && input.props){
-            //this is for react elements
-           _ReactDom.render(input,root)
-              
-            
-          }else{
-            root.innerHTML = JSON.stringify(input);
-          }
-        }
-        else{
-          root.innerHTML = input;
-        }
+    const showFn = `
+   import _React from 'react';
+   import _ReactDom from 'react-dom';
 
-      }
-      `,
-    ];
+   var show=(input)=>{
+     const root = document.querySelector("#root");
+     if(typeof input === 'object'){
+       if(input.$$typeof && input.props){
+         //this is for react elements
+        _ReactDom.render(input,root)
+           
+         
+       }else{
+         root.innerHTML = JSON.stringify(input);
+       }
+     }
+     else{
+       root.innerHTML = input;
+     }
+
+   }
+   `;
+    const showFnNoop = `var show=()=>{}`;
+    const cumulativeContent = [];
     for (const c of orderedCells) {
       if (c.type === "code") {
+        if (c.id === props?.id) {
+          cumulativeContent.push(showFn);
+        } else {
+          cumulativeContent.push(showFnNoop);
+        }
         cumulativeContent.push(c.content);
       }
       if (c.id === props?.id) {
@@ -55,6 +61,7 @@ const CodeCell = (props: Cell) => {
     return cumulativeContent;
   });
   const cumulativeContent = useSelector(getCumulativeContent);
+  console.log(cumulativeContent.join("\n"));
   useEffect(() => {
     const timer = setTimeout(async () => {
       const results: MyArgs = {
