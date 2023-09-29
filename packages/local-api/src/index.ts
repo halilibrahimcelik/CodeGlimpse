012fd9be6b7b454d551b180d64c8507d6bfc47e6
halilibrahimcelik <halil.celik139@gmail.com>
2023-09-29 17:32:45 +0300

    feat:added proxy for prod and dev environment.

--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,7 +1,28 @@
 import express from "express";
-
-export const serveLocalAPI = (port: number, filename: string, dir: string) => {
+import { createProxyMiddleware } from "http-proxy-middleware";
+import path from "path";
+export const serveLocalAPI = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
+
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware({
+        target: "http://127.0.0.1:5173",
+        ws: true,
+        logLevel: "silent",
+      })
+    );
+  } else {
+    const packagePath = require.resolve("codebook/dist/index.html");
+    //packagePath is the path to the index.html file in the codebook package in node_modules
+
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on("error", reject);
