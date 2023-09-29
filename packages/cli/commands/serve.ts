3e178cb78d204a59212bda7a0d68e8b7dab24615
halilibrahimcelik <halil.celik139@gmail.com>
2023-09-29 15:28:48 +0300

    feat:error handling and express fn added

--- packages/cli/commands/serve.ts
+++ packages/cli/commands/serve.ts
@@ -6,8 +6,15 @@ export const serveCommand = new Command()
   .command("serve [filename]")
   .description("Open a file for editing")
   .option("-p, --port <number>", "port to run server on", "4005")
-  .action((filename = "codeBook.js", options: { port: string }) => {
-    const directory = path.join(process.cwd(), path.dirname(filename));
-    console.log(path.basename(filename), "hello");
-    serveLocalAPI(parseInt(options.port), path.basename(filename), directory);
+  .action(async (filename = "codeBook.js", options: { port: string }) => {
+    try {
+      const directory = path.join(process.cwd(), path.dirname(filename));
+      await serveLocalAPI(
+        parseInt(options.port),
+        path.basename(filename),
+        directory
+      );
+    } catch (error: any) {
+      console.log("Heres the problem =>", error?.message);
+    }
   });
