87f1b1696f63b19f622f4d62c3881b25f8cd3390
halilibrahimcelik <halil.celik139@gmail.com>
2023-10-24 14:20:18 +0300

    feat: style bug fixed

--- packages/local-client/src/components/PreviwCode.tsx
+++ packages/local-client/src/components/PreviwCode.tsx
@@ -28,7 +28,11 @@ const PreviwCode: React.FC<Props> = ({ language, id }) => {
     <script src="https://cdn.tailwindcss.com"></script>
 
     </head>
-
+    <style>
+    body{
+     padding:20px;
+    }
+    </style>
     <body>
       <div id="root"></div>
       <div class="container mx-auto">
